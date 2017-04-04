3ac2d91c2e36e0cb595545973905f84276baef48
Michael Aschauer <m@ash.to>
2017-04-04 12:46:23 +0200

    more changes and fixes

--- static/js/turtlestitch.js
+++ static/js/turtlestitch.js
@@ -13,6 +13,11 @@ function buildHyperlinks (text) {
 
 $(document).ready(function(){
 	$(".logo").css("transform","rotate(" + Math.random() * 360 + "deg)");
-	$(".icon-run").css("transform","rotate(" + Math.random() * 360 + "deg)");
+	//$(".icon-run").css("transform","rotate(" + Math.random() * 360 + "deg)");
+	function rot_icon() {
+		$(".icon-run").css("transform","rotate(" + Math.random() * 360 + "deg)");
+		window.setTimeout( rot_icon, 3000 + Math.random() * 10000);
+	}
+	rot_icon();
 });
 
