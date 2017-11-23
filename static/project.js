ad785e328fb390de8212f733ee7b9d775881fab4
Michael Aschauer <m@ash.to>
2017-11-23 16:32:45 +0100

    comments and gravatar layout integration

--- static/project.js
+++ static/project.js
@@ -133,3 +133,122 @@ setAlternateImageSrc();
 sortThumbnails();
 
 document.getElementsByClassName('notes')[0].innerHTML = (buildHyperlinks(document.getElementsByClassName('notes')[0].innerHTML));
+
+
+
+function getComments (username, projectname) {
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            comments =JSON.parse(ajax.responseText);
+            updateComments();
+        }
+    };
+    path = '/api/users/'+ username + '/projects/' + projectname +'/comments';
+    ajax.open('GET', path, true);
+    ajax.send();
+}
+
+function getComment (id) {
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            comment =JSON.parse(ajax.responseText);
+            addComment(comment[0],true);
+        }
+    };
+    path = '/api/comment/' + id;
+    ajax.open('GET', path, true);
+    ajax.send();
+}
+
+function addComment(comment, prepend) {
+    comment_div = document.getElementById('comment-pool');
+    div = document.createElement('div');
+    div.setAttribute("id", "comment-" + comment.id);
+
+    if (!prepend)
+        comment_div.appendChild(div);
+    else {
+        comment_div.insertBefore(div, comment_div.firstChild);
+    }
+
+    div.innerHTML = "";
+
+    if ( comment.author == username || isadmin) {// if owner
+        div.innerHTML += '<div style="float:right"><a class=\"pure-button red-button\" role=\"button\" onclick=\"delete_comment('+
+            comment.id +' )\">Delete</a></div>';
+    }
+    div.innerHTML += '<div class="comment-item">' +
+        '<div class="pure-g"><div class="pure-u-1-5">' +
+        '<img width="48" height="48" class="gravatar" alt=""' +
+        ' src="http://www.gravatar.com/avatar/' + comment.gravatar + '?s=48&d=http%3A%2F%2Fbeta.turtlestitch.org%2Fstatic%2Flogo.png" />' +
+        '</div><div class="pure-u-4-5">' +
+        '<span class="author"><a href="/users/' +
+        comment.author + '">' + encodeURIComponent(comment.author) +
+        '</a></span><br /><p>' +
+        buildHyperlinks(comment.contents) + '</p>' +
+        '<span class="date">' +
+        moment(comment.date).fromNow() +
+        '</span><br />' +
+        '</ div>' +
+        '</ div><div class="clear"></div></div>';
+
+
+   //div.classList.add('flash');
+}
+
+function updateComments () {
+    if (comments.length) {
+        comments.forEach(
+            function (comment) {
+                addComment(comment, false);
+            }
+        );
+    }
+}
+
+function post_comment (projectname, author, username) {
+    var ajax_save_comment = new XMLHttpRequest();
+    ajax_save_comment.onreadystatechange = function () {
+        if (ajax_save_comment.readyState == 4 && ajax_save_comment.status == 200) {
+            document.getElementById('new_comment').value = "";
+            comments = JSON.parse(ajax_save_comment.responseText);
+            getComment(comments.comment.id);
+            document.getElementById('comments-count').innerHTML= parseInt(document.getElementById('comments-count').innerHTML) + 1;
+        }
+    };
+    ajax_save_comment.open('POST', '/api/comments/new', true);
+    ajax_save_comment.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    ajax_save_comment.send("projectname=" +
+        encodeURIComponent(projectname) +
+        "&author=" +
+        encodeURIComponent(author) +
+        "&projectowner=" +
+        encodeURIComponent(username) +
+        "&contents=" +
+        encodeURIComponent(document.getElementById('new_comment').value)
+        );
+}
+
+function delete_comment (id) {
+    message = 'You are about to delete this comment. <br /><br />' +
+            'Are you sure you want to continue?';
+    callback = function (result) {
+        if (!result) return;
+        var ajax = new XMLHttpRequest();
+        ajax.onreadystatechange = function () {
+            if (ajax.readyState == 4 && ajax.status == 200) {
+                ret = JSON.parse(ajax.responseText);
+                document.getElementById('comment-' + id).parentNode.removeChild(document.getElementById('comment-' + id));
+                document.getElementById('comments-count').innerHTML=parseInt(document.getElementById('comments-count').innerHTML) -1;
+            }
+        };
+        ajax.open('GET', '/api/comment/delete/' + id, true);
+        ajax.send();
+    };
+    
+	confirm(message, function() {callback(true)}, function() {return});
+}
+
+function reset_comment () {
+    document.getElementById('new_comment').value = '';
+}
